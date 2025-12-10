a33b0a466676ad841087fc1fb911f318dec170aa
Mathias Sass Michno <mathiassmichno@users.noreply.github.com>
2025-12-10 20:53:31 +0100

    Replace SteamWorks with sm-ripext for http requests (#4)
    * Add sm-ripext in dependencies directory
    
    * Add sm-ripext submodule
    * Update Dockerfile and dockerrunscript
    * Update github workflow build dependencies to ripext
    * Update travis with ripext dependency
    
    * Change http library from SteamWorks to REST in Pawn (sm-ripext)
    
    Remove dependency on SteamWorks extension for http request, by using
    sm-ripext instead. This also brings niceties such as https support, and
    no requirement of using a registered domain for URL match config loading.
    
    get5_apistats has not been updated - still requires SteamWorks

--- dockerrunscript.sh
+++ dockerrunscript.sh
@@ -3,4 +3,5 @@ cd /get5src && git submodule update --init
 cp -rf /get5src/* /get5/
 cd /get5
 cp -r ./dependencies/sm-json/addons/sourcemod/scripting/include/* ./addons/sourcemod/scripting/include
+cp -r ./dependencies/sm-ripext/pawn/scripting/include/* ./addons/sourcemod/scripting/include
 smbuilder --flags='-E'
